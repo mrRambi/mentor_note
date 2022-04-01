860eca36fb1bbe0303f5208195915c7ff2193d0a
mrRambi <maciej.beska1@gmail.com>
2022-04-01 21:48:11 +0200

    Added try-catch to fetchData function

--- lib/remote_data/remote_data.dart
+++ lib/remote_data/remote_data.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:http/http.dart' as http;
@@ -9,12 +10,22 @@ class NoteApi {
   static final _baseUrl =
       Uri.parse('https://mocki.io/v1/6a379166-05b1-4635-bd66-fafe35fa38d5');
   Future<List<Note>> fetchData() async {
-    final response =
-        await http.get(_baseUrl, headers: {"Content-Type": "aplication/json"});
-    final listOfJson = (jsonDecode(response.body) as List)
-        .map((e) => Note.fromJson(jsonEncode(e)))
-        .toList();
-    return listOfJson;
+    try {
+      final response = await http
+          .get(_baseUrl, headers: {"Content-Type": "aplication/json"});
+      if (response.statusCode != 200)
+        throw HttpException('${response.statusCode}');
+      final listOfJson = (jsonDecode(response.body) as List)
+          .map((e) => Note.fromJson(jsonEncode(e)))
+          .toList();
+      return listOfJson;
+    } on HttpException catch (e) {
+      print("HttpException $e");
+      rethrow;
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
   }
 }
 
